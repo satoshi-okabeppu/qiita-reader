babed66dc3980bc67dd77eb45f792145c39c8d3e
user <user>
2026-02-21 00:33:30 +0900

    デザイン修正

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import { useEffect, useState } from "react";
+import PullToRefresh from "react-pull-to-refresh";
 import "./App.css";
 
 function App() {
   const [articles, setArticles] = useState([]);
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
 
   const fetchArticles = () => {
     setLoading(true);
@@ -20,7 +21,12 @@ function App() {
         const sorted = data.sort(
         (a, b) => b.likes_count - a.likes_count
         );
+
         setArticles(sorted);
+        setLoading(false);
+              })
+      .catch((err) => {
+        console.error(err);
         setLoading(false);
       });
   };
@@ -30,27 +36,31 @@ function App() {
     fetchArticles();
   }, []);
 
-  if (loading) return <h2>読み込み中...</h2>;
+  if (loading) return <h2 style={{ textAlign: "center" }}>読み込み中...</h2>;
 
   return (
-    <div style={{ padding: "20px" }} name="bottom">
-      <h1>Qiita Reader</h1>
+    <PullToRefresh onRefresh={fetchArticles}>
+      <div name="container">
+        <div style={{ padding: "20px" }}>
+          <h1>Qiita Reader</h1>
 
-      {articles.map((article) => (
-        <div key={article.id} className="card">
-          <a href={article.url} target="_blank">
-            <h3>{article.title}</h3>
-          </a>
+          {articles.map((article) => (
+            <div key={article.id} className="card">
+              <a href={article.url} target="_blank">
+                <h3>{article.title}</h3>
+              </a>
 
-          <p>投稿者: {article.user.id}</p>
-          <p>いいね: {article.likes_count}</p>
+              <p>投稿者: {article.user.id}</p>
+              <p>いいね: {article.likes_count}</p>
+            </div>
+          ))}
         </div>
-      ))}
-      <div style={{ position: "fixed", bottom: "0", width: "96%", display: "flex", justifyContent: "space-between", padding: "10px", backgroundColor: "#f0f0f0" }}>
-        <button onClick={fetchArticles} disabled={loading}>更新</button>
-        <button onClick={() => { /* 検索処理をここに追加する */ }}>検索</button>
+        <div style={{ position: "fixed", bottom: "0", width: "96%", display: "flex", justifyContent: "space-between", padding: "10px", backgroundColor: "#f0f0f0" }}>
+          <button onClick={() => { /* 検索処理をここに追加する */ }}>検索</button>
+          <button onClick={() => { /* ブックマーク処理をここに追加する */ }}>後で</button>
+        </div>
       </div>
-    </div>
+    </PullToRefresh>
     
   );
 }
